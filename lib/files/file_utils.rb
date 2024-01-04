0a67e72fcd7893de93bdec3e5e6b2e8c04aa5a26
Jomy10 <jonas.vbs4@gmail.com>
2024-01-04 19:38:03 +0100

    filelist error handling

--- lib/files/file_utils.rb
+++ lib/files/file_utils.rb
@@ -8,7 +8,13 @@ module Beaver
     if filelist.is_a? String
       return Dir[filelist]
     elsif filelist.respond_to? :each
-      return filelist.flat_map { |expr| Dir[expr] }
+      return filelist.flat_map { |expr|
+        if expr.nil?
+          Beaver::Log::err("Found nil in file list #{filelist.inspect}")
+        else
+          Dir[expr]
+        end
+      }
     else
       Beaver::Log::err("Invalid filelist #{filelist.describe}")
     end
