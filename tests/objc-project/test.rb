aee85f08fd14bfd49cd4a3e7b15c7f71bbef4dbd
Jomy10 <jonas.vbs4@gmail.com>
2024-01-11 21:19:24 +0100

    fix objc for linux

--- tests/objc-project/test.rb
+++ tests/objc-project/test.rb
@@ -7,7 +7,8 @@ class ProjectWithFramework < Minitest::Test
   
   def test_run
     Dir.chdir(__dir__) do
-      assert_match /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} MyExecutable\[\d+:\d+\] Hello world\n/, `ruby make.rb run --no-verbose 2>&1`
+      assert_match /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} MyExecutable\[\d+:\d+\] Hello world\n/,
+        `ruby make.rb run --no-verbose 2>&1`
     end
   end
 end
