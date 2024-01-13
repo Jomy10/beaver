bfbc3dd16e01cd7d17e1570c82f6241968252a6f
Jomy10 <jonas.vbs4@gmail.com>
2024-01-13 14:03:46 +0100

    added windows? function to tests

--- tests/basic-c-commands/test.rb
+++ tests/basic-c-commands/test.rb
@@ -7,7 +7,7 @@ class BasicCCommand < Minitest::Test
 
   def test_build_default_command
     Dir.chdir(__dir__) do
-      if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
+      if windows? 
         system "set \"OUT=out\" && ruby make.rb"
       else
         system "OUT=out ruby make.rb"
@@ -19,46 +19,46 @@ class BasicCCommand < Minitest::Test
 
   def test_cache
     Dir.chdir(__dir__) do
-      if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
+      if windows? 
         system "set \"OUT=out\" && ruby make.rb"
       else
         system "OUT=out ruby make.rb"
       end
       assert_equal 0, $?.exitstatus
-      assert_equal "", `OUT=out ruby make.rb build`
-      assert_equal "Building object\nBuilding object\nLinking\n", `OUT=out ruby make.rb build -f`
+      assert_equal "", `#{windows? ? "set \"OUT=out\" &&" : "OUT=out"} ruby make.rb build`
+      assert_equal "Building object\nBuilding object\nLinking\n", `#{windows? ? "set \"OUT=out\" &&" : "OUT=out"} ruby make.rb build -f`
       assert_equal 0, $?.exitstatus
     end
   end
 
   def test_file_cache
     Dir.chdir(__dir__) do
-      if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
+      if windows? 
         system "set \"OUT=out\" && ruby make.rb"
       else
         system "OUT=out ruby make.rb"
       end
       assert_equal 0, $?.exitstatus
       FileUtils.rm_r "out/obj/main.o"
-      assert_equal "Building object\nBuilding object\nLinking\n", `OUT=out ruby make.rb build -f`
+      assert_equal "Building object\nBuilding object\nLinking\n", `#{windows? ? "set \"OUT=out\" &&" : "OUT=out"} ruby make.rb build -f`
       assert_equal 0, $?.exitstatus
       system "touch src/main.c"
-      assert_equal "Building object\nBuilding object\nLinking\n", `OUT=out ruby make.rb build -f`
+      assert_equal "Building object\nBuilding object\nLinking\n", `#{windows? ? "set \"OUT=out\" &&" : "OUT=out"} ruby make.rb build -f`
       assert_equal 0, $?.exitstatus
-      assert_equal "", `OUT=out ruby make.rb build`
+      assert_equal "", `#{windows? ? "set \"OUT=out\" &&" : "OUT=out"} ruby make.rb build`
       assert_equal 0, $?.exitstatus
     end
   end
 
   def test_run
     Dir.chdir(__dir__) do
-      if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
+      if windows?
         system "set \"OUT=out\" && ruby make.rb"
       else
         system "OUT=out ruby make.rb"
       end
       assert_equal 0, $?.exitstatus
-      assert_equal "Hello world!", `OUT=out ruby make.rb run`
+      assert_equal "Hello world!", `#{windows? ? "set \"OUT=out\" &&" : "OUT=out"} ruby make.rb run`
       assert_equal 0, $?.exitstatus
     end
   end
