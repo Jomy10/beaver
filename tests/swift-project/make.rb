860e496589b5898e763185af74dc9725eee9170f
Jomy10 <jonas.vbs4@gmail.com>
2024-01-17 20:23:29 +0100

    add ldflags to swift library

--- tests/swift-project/make.rb
+++ tests/swift-project/make.rb
@@ -3,7 +3,6 @@ require 'beaver'
 package = SPMProject.new(path: "TestPackage")
 package.targets["TestPackage"].flags.push(*["-Xswiftc", "-DHELLO"])
 
-system "pwd"
 Project.new("MyProject")
 
 exec = C::Executable.new(
@@ -12,10 +11,4 @@ exec = C::Executable.new(
   dependencies: ["TestPackage/TestPackage"],
   ldflags: []
 )
-
-require 'json'
-target_info = JSON.parse(`swiftc -print-target-info`)
-
-exec.ldflags.push(*target_info["paths"]["runtimeLibraryPaths"].map { |path| "-L#{path}" })
-exec.ldflags.push(*target_info["paths"]["runtimeLibraryImportPaths"].map { |path| "-L#{path}" })
 
--- tests/swift-project/test.rb
+++ tests/swift-project/test.rb
@@ -7,14 +7,14 @@ class Swift < Minitest::Test
       end
     end
   end
-
+  
   def test_run
     skip if linux? && !swift?
     Dir.chdir(__dir__) do
       assert_match /.*Hello from swift!\n$/, `ruby make.rb run`
     end
   end
-
+  
   def test_run_swift_executable
     skip if !swift?
     Dir.chdir(__dir__) do
