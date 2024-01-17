0a62106ddaf51ff5216c6b5938327ed4fb45b1b5
Jomy10 <jonas.vbs4@gmail.com>
2024-01-17 14:22:57 +0100

    this should work™

--- tests/swift-project/make.rb
+++ tests/swift-project/make.rb
@@ -12,24 +12,33 @@ exec = C::Executable.new(
   ldflags: []
 )
 
-case $beaver.host_os
-when :macos
-  if ENV["GH_ACTION"] == "1"
-    for root in ["Users/runner", "Applications", "/Library/Developer/CommandLineTools"]
-      exec.ldflags.push(*Dir["/#{root}/**/*swift*/**/macosx/"].map { |p| "-L#{p}"})
-      exec.ldflags.push(*Dir["/#{root}/**/*swift*/**/lib/"].map { |p| "-L#{p}"})
-    end
-  else
-    exec.ldflags << "-L/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/lib/swift/macosx"
-  end
-when :linux
-  if ENV["GH_ACTION"] == "1"
-    exec.ldflags.push(*Dir["/opt/**/lib"].map { |p| "-L#{p}"})
-    exec.ldflags.push(*Dir["/opt/**/lib/swift/linux"].map { |p| "-L#{p}"})
-    exec.ldflags.push(*Dir["/opt/**/lib/swift/host"].map { |p| "-L#{p}"})
-    exec.ldflags.push(*Dir["/opt/**/lib/swift_static/linux"].map { |p| "-L#{p}"})
-  else
-    exec.ldflags << "-L/usr/lib/swift"
-  end
-end
 
+system "swiftc -print-target-info"
+
+require 'json'
+target_info = JSON.parse(`swiftc -print-target-info`)
+
+exec.ldflags.push(*target_info["path"]["runtimeLibraryPaths"].map { |path| "-L#{path}" })
+exec.ldflags.push(*target_info["path"]["runtimeLibraryImportPaths"].map { |path| "-L#{path}" })
+
+# case $beaver.host_os
+# when :macos
+#   if ENV["GH_ACTION"] == "1"
+#     for root in ["Users/runner", "Applications", "/Library/Developer/CommandLineTools"]
+#       exec.ldflags.push(*Dir["/#{root}/**/*swift*/**/macosx/"].map { |p| "-L#{p}"})
+#       exec.ldflags.push(*Dir["/#{root}/**/*swift*/**/lib/"].map { |p| "-L#{p}"})
+#     end
+#   else
+#     exec.ldflags << "-L/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/lib/swift/macosx"
+#   end
+# when :linux
+#   if ENV["GH_ACTION"] == "1"
+#     exec.ldflags.push(*Dir["/opt/**/lib"].map { |p| "-L#{p}"})
+#     exec.ldflags.push(*Dir["/opt/**/lib/swift/linux"].map { |p| "-L#{p}"})
+#     exec.ldflags.push(*Dir["/opt/**/lib/swift/host"].map { |p| "-L#{p}"})
+#     exec.ldflags.push(*Dir["/opt/**/lib/swift_static/linux"].map { |p| "-L#{p}"})
+#   else
+#     exec.ldflags << "-L/usr/lib/swift"
+#   end
+# end
+#
