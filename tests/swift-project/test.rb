1bc9c047845ab0d9e2c6d95051fd762da2678f3f
Jomy10 <jonas.vbs4@gmail.com>
2024-01-17 19:58:56 +0100

    use skip instead of conditional importing in tests

--- tests/swift-project/test.rb
+++ tests/swift-project/test.rb
@@ -9,13 +9,14 @@ class Swift < Minitest::Test
   end
 
   def test_run
-    skip if linux?
+    skip if linux? && !swift?
     Dir.chdir(__dir__) do
       assert_match /.*Hello from swift!\n$/, `ruby make.rb run`
     end
   end
 
   def test_run_swift_executable
+    skip if !swift?
     Dir.chdir(__dir__) do
       assert_match /.*Running a swift executable from beaver\n$/, `ruby make.rb run TestPackage/TestExecutable`
     end
