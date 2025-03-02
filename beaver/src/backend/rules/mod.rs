dbc90c8683e93d4f4943fd5eeb5e937e23eea068
Jonas Everaert <jonas.vbs4@gmail.com>
2025-03-02 18:11:38 +0100

    Rule cmd paths

--- beaver/src/backend/rules/mod.rs
+++ beaver/src/backend/rules/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 use lazy_static::lazy_static;
 
 use crate::tools;
@@ -5,12 +7,13 @@ use crate::tools;
 use super::Rule;
 
 lazy_static! {
+    static ref CC_CMD: String = format!("{} $cflags -MD -MF $out.d -c $in -o $out", tools::cc.display());
     pub static ref CC: Rule = {
         Rule {
             name: "cc",
             options: vec![
                 ("description", "cc $in > $out"),
-                ("command", "cc $cflags -MD -MF $out.d -c $in -o $out"), // TODO: tools::cc + cc_extra_args
+                ("command", &CC_CMD),
                 ("deps", "gcc"),
                 ("depfile", "$out.d")
             ]
